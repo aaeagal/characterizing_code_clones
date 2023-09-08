fea24c28cb3527155c65ba55aa6666a629257b94
Azeeza Eagal <90269083+aaeagal@users.noreply.github.com>
2023-09-08 18:15:39 -0400

    Prompting for process one done (#1)
    * starting over
    
    * will get all of these done TODAY! SLACC tomorrow!
    
    * done with 5 nl prompts
    
    * ten
    
    * fifteen DONE
    
    * eated those pancakes: 20
    
    * 25 initial prompts differ
    
    * 30 prompts are done
    
    ---------
    
    Co-authored-by: Azeeza Eagal <aeagal@eb2-3296-fal04.csc.ncsu.edu>

---
+++ collect_samples.py
@@ -0,0 +1,74 @@
+# Collect 20 samples of code from openai give NL
+import hashlib
+import os
+import sys
+import json
+import random
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+
+def get_code(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {
+            "role": "user",
+            "content": f'{prompt}\n'
+        }
+        ],
+        temperature=1,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response["choices"][0]["message"]["content"]
+
+def main():
+    # prompt for code samples
+    prompt = "An additive number is a string whose digits can form an additive sequence. A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two. Write a Java function that given a string containing only digits, return true if it is an additive number or false otherwise. Constraints: 1 <= num.length <= 35, num consists only of digits. Do not provide an explanation. Just give me the source code."
+    prompt_id = "additiveNumber30"
+   # Create a directory to store the samples
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    # Create a file to store the samples
+    with open(f"data/nl_prompt/{prompt_id}.Java", "w") as f:
+        #write the prompt to the file & add a new line
+        #f.write(prompt + "\n")
+
+        #counter for the number of unique samples and total samples
+        unique_samples = 0
+        total_samples = 0
+
+        #set of hashes for the samples
+        hashes = set()
+
+
+        #loop until we have 20 unique samples
+        while unique_samples < 20:
+            #increment the number of samples
+            total_samples += 1
+
+            #get the code
+            code = get_code(prompt)
+            print("Code: ", code)
+
+            #hash the code
+            code_hash = hashlib.sha256(code.encode()).hexdigest()
+
+            #check if the code is unique
+            if code_hash not in hashes:
+               print("Unique code")
+                #write the code to the file
+               f.write(code + "\n")
+               unique_samples += 1
+               hashes.add(code_hash)
+        print(f"Total samples: {total_samples}")
+        print(f"Unique samples: {unique_samples}")
+
+
+if __name__ == "__main__":
+    main()
